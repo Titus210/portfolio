5a29f5bae9e3af1b72983d16b05b34b71ad66477
Titus210 <tituskiplagat210@gmail.com>
2023-09-01 22:48:32 +0300

    feat: Pages layout setup

---
+++ src/components/layout/ContactLayout.tsx
@@ -0,0 +1,9 @@
+const ContactLayout = () => {
+    return (
+        <>
+            <h1>This is contact layout</h1>
+        </>
+    );
+}
+
+export default ContactLayout;
---
+++ src/components/layout/LandingLayout.tsx
@@ -0,0 +1,9 @@
+const LandingLayout = () => {
+    return (
+        <>
+            <h1>Hello Landing layout</h1>
+        </>
+    );
+}
+
+export default LandingLayout;
