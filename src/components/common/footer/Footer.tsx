ee9dfe2dde8c95a7a8d61c380197a173dce2b48a
Titus210 <tituskiplagat210@gmail.com>
2023-09-04 13:54:19 +0300

    [Header/Feat] : Add Footer Component
    [Body]:
    - Implemented the Footer component that represents the footer section of the web page.
    - Added the developer's name and a brief description of their role as a frontend web developer.
    - Included social media icons for LinkedIn, GitHub, and Hashnode.
    - Applied styling and hover effects to the social media icons for interactivity.
    - Updated component structure to ensure responsiveness on various screen sizes.
    
    [What's Fixed] : N/A
    
    [ Remaining Tasks] :
    - Consider adding links to the social media profiles for increased user engagement.
    - Ensure consistent implementation of hover effects across different browsers.
    
    Authored-by: Titus Kiplagat

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,7 +1,30 @@
+import { PiGithubLogoFill, PiLinkedinLogo } from "react-icons/pi";
+import { SiHashnode } from "react-icons/si";
+
 const Footer = () => {
     return (
         <>
-            <h1>This is footer</h1>
+            <div className="bg-secondaryBg   py-4">
+                <div className="flex flex-col   mx-auto justify-between items-center md:flex-row  md:w-8/12">
+                    <div className="text w-9/12 md:w-7/12">
+                        <h2 className="text-secondary font-semibold text-xl">Titus Kiplagat</h2>
+                        <p className="text-md text-white">A frontend web developer Bulding FrontEnd Websites and Web Applications</p>
+                    </div>
+                    <div className="socials flex  items-center mt-5 md:mt-0">
+                        <div className="linkedIn flex items-center justify-center overflow-hidden rounded-full h-12 w-12 bg-slate-500">
+                            <PiLinkedinLogo className="w-7 h-7 hover:scale-125" />
+                        </div>
+                        <div className="github mx-4 flex items-center justify-center overflow-hidden rounded-full h-12 w-12 bg-slate-500">
+                            <PiGithubLogoFill className="w-7 h-7 hover:scale-125" />
+                        </div>
+                        <div className="hashnode flex items-center justify-center overflow-hidden rounded-full h-12 w-12 bg-slate-500">
+                            <SiHashnode className="w-7 h-7 hover:scale-125" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </>
     );
 }
