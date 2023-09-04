310fcf026534660023dbe83649958510c8a3dd07
Titus210 <tituskiplagat210@gmail.com>
2023-09-04 14:32:47 +0300

    Header/Feat: Create LandingLayout Component
    Body:
    - Implemented the LandingLayout component to structure the landing page.
    - Organized sections for Hero, About, Projects, Blogs, and Contact.
    - Utilized specific components (e.g., LandingHero, LandingAbout) for each section.
    - Styled section headings and content with appropriate margins and styling.
    
    What's Fixed: N/A
    
    Remaining Tasks:
    - Populate each section with dynamic content as needed (e.g., actual text, project details).
    
    Co-authored-by: Titus Kiplagat

--- src/components/layout/landingLayout/LandingLayout.tsx
+++ src/components/layout/landingLayout/LandingLayout.tsx
@@ -6,13 +6,39 @@ import LandingContact from "./LandingContact";
 import LandingHero from "./LandingHero";
 import LandingProjects from "./LandingProjects";
 
+
+/**
+ * LandingLayout Component:
+ *
+ * The LandingLayout component represents the layout structure of the landing page. It includes sections for:
+ *
+ * - Hero (Introduction)
+ * - About
+ * - Projects
+ * - Blogs
+ * - Contact
+ *
+ * Key Points:
+ * - Each section corresponds to a specific component (e.g., LandingHero, LandingAbout).
+ * - Section headings and content are organized with appropriate margins and styling.
+ *
+ * Note:
+ * - The "Expertise" section is currently commented out but can be uncommented if needed.
+ *
+ * Suggestions:
+ * - Populate each section with dynamic content as necessary (e.g., actual text, project information).
+ *
+ * Overall, the component provides a structured layout for the landing page.
+ */
+
+
 const LandingLayout = () => {
     return (
         <>
             <main className="">
                 <div>
                     {/*Hero*/}
-                    <section className="background-2 bg-secondaryBg flex justify-center items-center ">
+                    <section className="bg-primaryBg flex justify-center items-center ">
                         <LandingHero />
                     </section>
 
@@ -35,6 +61,7 @@ const LandingLayout = () => {
                     <section className="bg-primaryBg py-6 h-full">
                         <div className="flex items-center  justify-center mb-6 md:mb-12">
                             <Heading headingText="Projects" />
+
                         </div>
                         <div className="page-container   flex items-center justify-center">
                             <LandingProjects />
@@ -42,7 +69,7 @@ const LandingLayout = () => {
                     </section>
 
                     {/* Blogs */}
-                    <section className="bg-secondaryBg py-6">
+                    <section className="bg-secondaryBg py-6  h-full">
                         <div className="flex items-center  justify-center mb-6 md:mb-12">
                             <Heading headingText="My Blogs" />
                         </div>
@@ -52,8 +79,16 @@ const LandingLayout = () => {
                     </section>
 
                     {/* Contact */}
-                    <section className="bg-primaryBg">
-                        <LandingContact />
+                    <section className="bg-primaryBg h-max py-8">
+                        <div className="flex flex-col items-center  justify-center mb-6 md:mb-12">
+                            <Heading headingText="Contact" />
+                            <h2 className="text-sm mt-2">
+                                Feel free to get in touch
+                            </h2>
+                        </div>
+                        <div className="page-container flex items-center justify-center">
+                            <LandingContact />
+                        </div>
                     </section>
                 </div>
             </main>
