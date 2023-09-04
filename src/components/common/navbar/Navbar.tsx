118756164a7737c9d8d17f606f9993f296559d92
Titus210 <tituskiplagat210@gmail.com>
2023-09-04 14:00:03 +0300

    Header/Feat: Create Navbar Component
    Body:
    - Implemented the Navbar component for the website's navigation bar.
    - Added a logo with the text "Kiplagat" for branding.
    - Included navigation links for sections like Home, About, Expertise, Projects, and Contact.
    - Ensured that the navigation menu is responsive, displaying as a dropdown on smaller screens.
    - Added menu button icons (BiMenuAltRight for opening and AiOutlineClose for closing) for mobile navigation.
    - Applied styles and hover effects for improved interactivity.
    
    What's Fixed: N/A
    
    Remaining Tasks:
    - Replace placeholder link destinations with actual URLs.
    - Verify and fine-tune the functionality of the mobile menu.
    - Consider adding transition animations for smoother user interactions.
    
    Co-authored-by: Titus Kiplagat

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -7,17 +7,17 @@ import { AiOutlineClose } from 'react-icons/ai';
 const Navbar = () => {
     return (
         <>
-            <header className="bg-slate-600  md:p-4 " >
-                <div className="logo flex justify-between p-2 mx-auto relative lg:w-8/12">
+            <header className="bg-secondaryBg  md:p-4 " >
+                <div className="logo flex justify-between p-2 mx-auto relative lg:w-11/12">
                     <div className="logo flex ">
                         <h1 className="text-2xl font-bold">Kiplagat</h1>
                     </div>
                     <nav className="hidden flex-col p-4 right-0 top-0 text-center  absolute w-7/12 h-72 justify-center bg-primaryLight shadow-sm lg:flex md:flex-row md:bg-inherit md:w-12/12 md:p-0 md:h-max">
-                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2 hover:bg-slate-300 md:mb-0"><Link to="/">Home</Link></p>
-                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2 hover:bg-slate-300 md:mb-0"><Link to="/">About</Link></p>
-                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2 hover:bg-slate-300 md:mb-0"><Link to="/">Expertise</Link></p>
-                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2 hover:bg-slate-300 md:mb-0"><Link to="/">Projects</Link></p>
-                        <p className="text-text-primary font-bold text-xl p-2 hover:bg-slate-300 md:mb-0"><Link to="/">Contact</Link></p>
+                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2  hover:text-primary md:mb-0"><Link to="/">Home</Link></p>
+                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2  hover:text-primary md:mb-0"><Link to="/">About</Link></p>
+                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2  hover:text-primary md:mb-0"><Link to="/">Expertise</Link></p>
+                        <p className="text-text-primary font-bold text-xl mb-4 md:mr-6 p-2  hover:text-primary md:mb-0"><Link to="/">Projects</Link></p>
+                        <p className="text-text-primary font-bold text-xl p-2  hover:text-primary md:mb-0"><Link to="/">Contact</Link></p>
                     </nav>
                 </div>
                 <div className="menu-bar absolute top-0 right-0 p-2 md:hidden">
