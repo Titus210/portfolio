e387a25da23bb27ce91267aafc9ec823cf4846b0
Titus210 <tituskiplagat210@gmail.com>
2023-10-08 23:21:50 +0300

    Feat: Set up Contact Page Layout
    - Created the ContactLayout component for the Contact page.
    - Integrated ContactInfo component to display contact details.
    - Integrated ContactInput component to render a contact input form.
    - Designed the layout to be responsive, adjusting to different screen sizes.
    - Positioned Contact Information and Contact Form side by side on larger screens and stacked on smaller screens.
    
    Fix Done: Contact Page Layout has been successfully implemented.

--- src/components/layout/contactLayout/ContactLayout.tsx
+++ src/components/layout/contactLayout/ContactLayout.tsx
@@ -1,7 +1,38 @@
+import ContactInfo from "../../ui/cards/contact/ContactInfo";
+import ContactInput from "../../ui/inputs/ContactForm";
+
+/**
+ * ContactLayout Component:
+ *
+ * The ContactLayout component represents the layout of the Contact page, which includes two main sections:
+ *
+ * 1. Contact Information: Displays contact details using the ContactInfo component.
+ * 2. Contact Form: Displays a contact input form using the ContactInput component.
+ *
+ * Key Points:
+ * - The layout is designed to be responsive, adjusting to different screen sizes.
+ * - Contact Information and Contact Form are displayed side by side on larger screens and stacked on smaller screens.
+ *
+ * Suggestions:
+ * - Ensure that the layout maintains a balanced design across different screen sizes.
+ * - Check responsiveness by testing on various devices.
+ *
+ * Overall, the component efficiently organizes contact-related content and maintains responsiveness.
+ */
+
 const ContactLayout = () => {
     return (
         <>
-            <h1>This is contact layout</h1>
+            <div className="w-full flex justify-evenly h-screen">
+                <div className="flex w-11/12 justify-evenly flex-col-reverse items-center md:flex-row-reverse">
+                    <div className="contact-info mr-12">
+                        <ContactInfo />
+                    </div>
+                    <div className="contact-info w-11/12 md:w-6/12 lg:w-4/12">
+                        <ContactInput />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
